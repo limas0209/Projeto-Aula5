3e60034ecbb7d19381945584bc3300228c0f5ec9
Henrique <riquelima00@gmail.com>
2023-09-19 11:45:22 -0300

    Projeto Aula 5

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,8 @@
+export const ListaProdutos = [
+    {id: 1, nome: "Teclado Gamer", desc: "Teclado mecanico com leds RGB", preco: 200},
+    {id: 2, nome: "Mouse Gamer", desc: "Mouse Gamer com leds RGB, 10000DPI", preco: 220},
+    {id: 3, nome: "Cadeira Gamer", desc: "Cadeira gamer ergonomica", preco: 1500},
+    {id: 4, nome: "Headset Gamer", desc: "HeadSet 7.1 estéreo com leds RGB", preco: 200},
+    {id: 5, nome: "Monitor Gamer", desc: "Monitor Gamer 144Hz 21 polegadas freesync", preco: 200},
+    {id: 6, nome: "Mousepad Gamer", desc: "MousePad liso e costurado", preco: 150},
+]
